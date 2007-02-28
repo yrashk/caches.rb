244471cfa5c90f2448f3883ebb60a26ab4dec19b
yrashk <yrashk@f28541f9-331e-0410-82d8-e175c77b79a1>
2007-02-28 15:40:14 +0000

    ActiveRecord models use ClassVarById storage by default
    git-svn-id: http://svn.verbdev.com/rb/caches.rb/trunk@50 f28541f9-331e-0410-82d8-e175c77b79a1

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@ require 'caches'
 
 begin
 	Kernel.const_get :ActiveRecord
-	ActiveRecord::Base.extend Caches
+  ActiveRecord::Base.extend CachesConfig
+  ActiveRecord::Base.extend Caches
+  ActiveRecord::Base.caches_storage = CachesStorage::ClassVarById
 rescue
 end	
