95b0fcdfa49d5ce40e9eb2a479fc31d3152537b3
yrashk <yrashk@f28541f9-331e-0410-82d8-e175c77b79a1>
2007-04-08 13:11:47 +0000

    ActiveRecord workaround
    git-svn-id: http://svn.verbdev.com/rb/caches.rb/trunk@70 f28541f9-331e-0410-82d8-e175c77b79a1

--- lib/caches.rb
+++ lib/caches.rb
@@ -7,32 +7,6 @@ begin
 rescue MissingSourceFile, LoadError
 end
 module Caches
-  module ActiveRecordWorkaround
-
-    def remove_methods_on_reset?
-      false
-    end
-
-    def remove_variables_on_reset?
-      false
-    end
-    
-    def reset_subclasses #:nodoc:
-      nonreloadables = []
-      subclasses.each do |klass|
-        unless Dependencies.autoloaded? klass
-          nonreloadables << klass
-          next
-        end
-        klass.instance_variables.each { |var| klass.send(:remove_instance_variable, var) } if klass.remove_variables_on_reset?
-        klass.instance_methods(false).each { |m| klass.send :undef_method, m } if klass.remove_methods_on_reset?
-      end
-      @@subclasses = {}
-      nonreloadables.each { |klass| (@@subclasses[klass.superclass] ||= []) << klass }
-    end
-
-  end
-
 
   module Helper
 
