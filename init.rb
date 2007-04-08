e823621b654648daeafc73623f5f8959befabacb
yrashk <yrashk@f28541f9-331e-0410-82d8-e175c77b79a1>
2007-04-08 13:04:33 +0000

    ActiveRecord workaround
    git-svn-id: http://svn.verbdev.com/rb/caches.rb/trunk@69 f28541f9-331e-0410-82d8-e175c77b79a1

--- init.rb
+++ init.rb
@@ -3,10 +3,11 @@ require 'caches'
 begin
 	Kernel.const_get :ActiveRecord
   ActiveRecord::Base.extend Caches
+  ActiveRecord::Base.extend Caches::ActiveRecordWorkaround
   ActiveRecord::Base.instance_cache_storage Caches::Storage::Class
   ActiveRecord::Base.class_eval do
     include Caches::Helper::PerID
-    include Caches::ActiveRecordWorkaround
   end
+  
 rescue
 end	
