33d91a14cb13e17a18d964cd48d0e55c9322d0c1
yrashk <yrashk@f28541f9-331e-0410-82d8-e175c77b79a1>
2007-04-08 13:24:38 +0000

    ActiveRecord workaround (another one)
    git-svn-id: http://svn.verbdev.com/rb/caches.rb/trunk@71 f28541f9-331e-0410-82d8-e175c77b79a1

--- init.rb
+++ init.rb
@@ -7,27 +7,27 @@ begin
   ActiveRecord::Base.class_eval do
     include Caches::Helper::PerID
 
-    def self.remove_methods_on_reset?
-      false
-    end
-
-    def self.remove_variables_on_reset?
-      false
-    end
-
-    def self.reset_subclasses #:nodoc:
-      nonreloadables = []
-      subclasses.each do |klass|
-        unless Dependencies.autoloaded? klass
-          nonreloadables << klass
-          next
-        end
-        klass.instance_variables.each { |var| klass.send(:remove_instance_variable, var) } if klass.remove_variables_on_reset?
-        klass.instance_methods(false).each { |m| klass.send :undef_method, m } if klass.remove_methods_on_reset?
-      end
-      @@subclasses = {}
-      nonreloadables.each { |klass| (@@subclasses[klass.superclass] ||= []) << klass }
-    end
+    # def self.remove_methods_on_reset?
+    #   false
+    # end
+    # 
+    # def self.remove_variables_on_reset?
+    #   false
+    # end
+    # 
+    # def self.reset_subclasses #:nodoc:
+    #   nonreloadables = []
+    #   subclasses.each do |klass|
+    #     unless Dependencies.autoloaded? klass
+    #       nonreloadables << klass
+    #       next
+    #     end
+    #     klass.instance_variables.each { |var| klass.send(:remove_instance_variable, var) } if klass.remove_variables_on_reset?
+    #     klass.instance_methods(false).each { |m| klass.send :undef_method, m } if klass.remove_methods_on_reset?
+    #   end
+    #   @@subclasses = {}
+    #   nonreloadables.each { |klass| (@@subclasses[klass.superclass] ||= []) << klass }
+    # end
 
   end
 
